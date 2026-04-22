d1212e26144148aa3ff0a9038b5f7b84ea8edec8
Brandon Zhang <nickletart@gmail.com>
2026-04-21 19:52:15 -0500

    switch files

--- Switch.cpp
+++ Switch.cpp
@@ -1,23 +1,11 @@
-/*
- * Switch.cpp
- *
- *  Created on: Nov 5, 2023
- *      Author:
- */
 #include <ti/devices/msp/msp.h>
 #include "../inc/LaunchPad.h"
-// LaunchPad.h defines all the indices into the PINCM table
+
 void Switch_Init(void){
-    // write this
-  IOMUX->SECCFG.PINCM[PA28INDEX] = 0x00040081; // input, no pull
-  IOMUX->SECCFG.PINCM[PA27INDEX] = 0x00040081; // input, no pull
-  IOMUX->SECCFG.PINCM[PA16INDEX] = 0x00040081; // input, no pull
-  IOMUX->SECCFG.PINCM[PA15INDEX] = 0x00040081; // input, no pull
+  IOMUX->SECCFG.PINCM[PA24INDEX] = 0x00040081;
+  IOMUX->SECCFG.PINCM[PA25INDEX] = 0x00040081;
 }
-// return current state of switches
-uint32_t Switch_In(void){
-    // write this
-  uint32_t data = GPIOA->DIN31_0;
-  data = ((data>>15)&0x03) | ((data&((1<<28)|(1<<27)))>>25);
-  return data; // return 0; //replace this your code
+
+int Switch_In(void){
+  return GPIOA->DIN31_0>>24 & 3;
 }
